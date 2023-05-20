c96d4544d6ef4d20679ce8839127c2651368b76f
HobbesChiong <93816506+HobbesChiong@users.noreply.github.com>
2023-05-20 16:07:02 -0700

    get game data and giving a point works

--- Api/main.py
+++ Api/main.py
@@ -5,13 +5,13 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Firebase stuff
-cred = credentials.Certificate("apiJSON\stormhacks2023-1fe1cfda32c0.json")
+cred = credentials.Certificate("apiJSON\stormhacks2023-firebase-adminsdk-dbj0e-bb5d4d40b8.json")
 
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-collection_main = db.collection("games")
+collection_games = db.collection("games")
 
 
 # FastAPI stuff
@@ -28,35 +28,21 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-# @app.get("/characters")
-# async def get_chars():
-#     # return the characters
-#     return 
-
 # returns a game
 @app.get("/games/{seshID}")
-async def get_game(seshID):
+async def get_game(seshID: str):
     try:
-        print(seshID)
-        doc = await db.collection('games').document('test').get()
-        print('hahha')
-        if (doc.exists == True):
-            print('asdfasdf')
-        else: 
-            print('asdfasdf12341243')
-        print(doc)
+        doc = collection_games.document(seshID).get()
         return JSONResponse(content=doc.to_dict(), status_code=200)
     except Exception as e:
         raise HTTPException(status_code=404, detail="Game not found")
     
 # give a player a point in the current game
-# do type annotations later
-# @app.post("/games/{seshID}/{userID}", status_code=200)
-# async def give_point(seshID: str ,userID:str):
-#     try:
-#         document = collection_games.document(seshID)
-#         document.update({userID: firestore.Increment(1)})
-
-#     except Exception as e:
-#         print("error lol")
-#         raise HTTPException(status_code=404, detail="Item ID not found")
+@app.post("/games/{seshID}/{userID}", status_code=200)
+async def give_point(seshID: str ,userID:str):
+    try:
+        document = collection_games.document(seshID)
+        document.update({userID: firestore.Increment(1)})
+    except Exception as e:
+        print("error lol")
+        raise HTTPException(status_code=404, detail="Item ID not found")
