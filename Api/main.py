98e31b6f31830abc5532605891a744b1181fdb9f
HobbesChiong <93816506+HobbesChiong@users.noreply.github.com>
2023-05-20 15:33:05 -0700

    not working

--- Api/main.py
+++ Api/main.py
@@ -5,13 +5,13 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Firebase stuff
-cred = credentials.Certificate("apiJSON/stormhacks-api-cf5d066f8e25.json")
+cred = credentials.Certificate("apiJSON\stormhacks2023-1fe1cfda32c0.json")
 
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-collection_games = db.collection('games')
+collection_main = db.collection("games")
 
 
 # FastAPI stuff
@@ -23,10 +23,10 @@ app = FastAPI()
 # http://127.0.0.1:8000
 
 
-# Example
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 # @app.get("/characters")
 # async def get_chars():
@@ -34,22 +34,29 @@ app = FastAPI()
 #     return 
 
 # returns a game
-@app.get("/game/{seshID}")
+@app.get("/games/{seshID}")
 async def get_game(seshID):
     try:
-        doc = collection_games.document(seshID).get()
+        print(seshID)
+        doc = await db.collection('games').document('test').get()
+        print('hahha')
+        if (doc.exists == True):
+            print('asdfasdf')
+        else: 
+            print('asdfasdf12341243')
+        print(doc)
         return JSONResponse(content=doc.to_dict(), status_code=200)
     except Exception as e:
         raise HTTPException(status_code=404, detail="Game not found")
     
 # give a player a point in the current game
 # do type annotations later
-@app.post("/game/{seshID}/{userID}", status_code=200)
-async def give_point(seshID,userID):
-    try:
-        document = collection_games.document(seshID)
-        document.update({userID: firestore.Increment(1)})
+# @app.post("/games/{seshID}/{userID}", status_code=200)
+# async def give_point(seshID: str ,userID:str):
+#     try:
+#         document = collection_games.document(seshID)
+#         document.update({userID: firestore.Increment(1)})
 
-    except Exception as e:
-        print("error lol")
-        raise HTTPException(status_code=404, detail="Item ID cannot be negative")
+#     except Exception as e:
+#         print("error lol")
+#         raise HTTPException(status_code=404, detail="Item ID not found")
