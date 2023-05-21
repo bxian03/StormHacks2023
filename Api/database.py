018730a79d6e6ec95af54375ca43b9489ea2b609
Brian Xian <brianxian2607@gmail.com>
2023-05-20 22:21:50 -0700

    i don't know what happened

---
+++ Api/database.py
@@ -0,0 +1,23 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+def get_database_connection():
+
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("./apiJSON/stormhacks2023-firebase-adminsdk-dbj0e-bb5d4d40b8.json")
+        firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
+    try:
+        yield db
+    finally:
+        pass
+
+# def get_collection_leaderboard():
+#     db = get_database_connection()
+#     print(db)
+#     collection_leaderboard = db.collection("Leaderboard")
+#     try:
+#         yield collection_leaderboard
+#     finally:
+#         pass
--- Api/main.py
+++ Api/main.py
@@ -1,25 +1,26 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import JSONResponse
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 import random
 
 from routes import characters
+from database import get_database_connection
 
-# Firebase stuff
-cred = credentials.Certificate("./apiJSON/stormhacks2023-firebase-adminsdk-dbj0e-bb5d4d40b8.json")
+def get_collection_leaderboard(db = Depends(get_database_connection)):
+    return db.collection("Leaderboard")
 
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-
-collection_leaderboard = db.collection("Leaderboard") 
-collection_questions = db.collection("questions")
-
+def get_collection_questions(db = Depends(get_database_connection)):
+    return db.collection("characters")
 
 # FastAPI stuff
-app = FastAPI()
+class Room: 
+    def __init__(self,sessionID,user1WebSocket,user2WebSocket):
+        self.sessionID = sessionID
+        self.user1Ready = False
+        self.user2Ready = False
+        self.user1WebSocket = user1WebSocket
+        self.user2WebSocket = user2WebSocket
+
+app = FastAPI(dependencies=[Depends(get_database_connection), Depends(get_collection_leaderboard), Depends(get_collection_questions)])
 
 app.include_router(characters.router)
 
@@ -36,7 +37,7 @@ async def root():
 
 # get player score
 @app.get("/Leaderboard/Points/{userID}", status_code=200)
-async def get_player_score(userID: str):
+async def get_player_score(userID: str, collection_leaderboard = Depends(get_collection_leaderboard)):
     try:
         doc_ref = collection_leaderboard.document("Points").get()
         doc_dict = doc_ref.to_dict()
@@ -48,7 +49,7 @@ async def get_player_score(userID: str):
 
 # add to player score
 @app.post("/Leaderboard/Points/{userID}/{points}", status_code=200)
-async def add_player_score(userID: str, points: int):
+async def add_player_score(userID: str, points: int, collection_leaderboard = Depends(get_collection_leaderboard)):
     try:
         doc = collection_leaderboard.document("Points").get()
         doc_dict = doc.to_dict()
@@ -71,7 +72,7 @@ async def add_player_score(userID: str, points: int):
 # supports hiragana katakana json files
 # answer is the answer to the question
 @app.get("/questions/{questionDocument}/{questionKey}/{answer}", status_code=200)
-async def validate_answer(questionDocument: str, questionKey: str, answer: str): 
+async def validate_answer(questionDocument: str, questionKey: str, answer: str, collection_leaderboard = Depends(get_collection_leaderboard)): 
     try:
         answer = answer.lower()
         doc_ref = collection_leaderboard.document(questionDocument).get()
@@ -89,7 +90,7 @@ async def validate_answer(questionDocument: str, questionKey: str, answer: str):
 # returns 5 random questions from the document in the form of a list with 5 key value pairs
 # [(question, answer), (question, answer), (question, answer), (question, answer), (question, answer))]
 @app.get("/questions/{questionDocument}/", status_code=200)
-async def get_questions(questionDocument: str):
+async def get_questions(questionDocument: str, collection_questions = Depends(get_collection_questions)):
     try:
         doc_ref = collection_questions.document(questionDocument).get()
         doc_dict = doc_ref.to_dict()
