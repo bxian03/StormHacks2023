8aa0fa940f66c0c0b1ef1c68053342160fbcbc7b
vinn03 <you@example.com/Users/vincentliu/Documents/HackToSchoolBot22/.venv/bin/python>
2023-05-20 21:08:12 -0700

    changed fonts, colours of most elements. Added an icon and formatted margins a little.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Player from "../utils/Player";
 import { createRoom } from "../utils/createRoom";
 import InputName from "../components/InputName";
 import { Button } from "@mui/base";
+import japangologo from "../../public/images/japangologo.png"
+import WebFont from 'webfontloader';
 
 import {
   TextField,
@@ -25,8 +27,15 @@ export default function Home() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-center text-white selection:bg-green-900">
-      <h1 className="text-5xl">Japanago</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FFFACD] text-center text-black selection:bg-green-900">
+      <h1 className="text-6xl font-serif">Japango</h1>
+
+      <div className="py-8">
+      <img className="" style={{ width: 300, height: 300 }} src={japangologo} alt="Japango Logo"/>
+      </div>
+
+      <h2 className="text-4xl font-serif">Please Enter a name:</h2>
+      <h2 className="text-2xl font-serif">お名前を入力してください：</h2>
       <form
         onSubmit={async (e) => {
           e.preventDefault();
--- frontend/src/pages/Kanji.jsx
+++ frontend/src/pages/Kanji.jsx
@@ -7,8 +7,8 @@ export default function Kanji() {
   // let questionNum = 0;
   const [answer, setAnswer] = useState("");
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-center text-white selection:bg-green-900">
-      <h1 className="text-5xl">Test Your Kanji Knowledge</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FFFACD] text-center text-black selection:bg-green-900">
+      <h1 className="text-5xl font-serif py-8">Test Your Kanji Knowledge</h1>
       {/* <img src={questions[questionNum].image} alt="kanji" /> */}
       <TextField value={answer} onChange={(e) => setAnswer(e.target.value)} />
     </div>
