46b66041ffee01c5f29d7a7f364dde2de1ff5ca8
vinn03 <you@example.com/Users/vincentliu/Documents/HackToSchoolBot22/.venv/bin/python>
2023-05-20 21:36:08 -0700

    added more gamemode buttons, spaced out textfields, changed highlight color, changed in-field text, added hiragana.jsx

--- frontend/src/pages/Hiragana.jsx
+++ frontend/src/pages/Hiragana.jsx
@@ -2,13 +2,13 @@ import { TextField } from "@mui/material";
 import React, { useState } from "react";
 import { useLoaderData } from "react-router";
 
-export default function Kanji() {
+export default function Hiragana() {
   const questions = useLoaderData();
   let questionNum = 0;
   const [answer, setAnswer] = useState("");
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FFFACD] text-center text-black selection:bg-green-900">
-      <h1 className="text-5xl font-serif py-8">Test Your Kanji Knowledge</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FFFACD] text-center text-black">
+      <h1 className="text-5xl font-serif py-8">Test Your Hiragana Knowledge</h1>
       {/* <img src={questions[questionNum].image} alt="kanji" /> */}
       <TextField value={answer} onChange={(e) => setAnswer(e.target.value)} />
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -28,14 +28,22 @@ export default function Home() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FFFACD] text-center text-black selection:bg-green-900">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#FFFACD] text-center text-black">
       <h1 className="text-6xl font-serif">Japango</h1>
 
       <div className="py-8">
-      <img className="" style={{ width: 300, height: 300 }} src={japangologo} alt="Japango Logo"/>
+        <img className="" style={{ width: 300, height: 300 }} src={japangologo} alt="Japango Logo"/>
       </div>
 
-      <h2 className="text-4xl font-serif">Please Enter a name:</h2>
+      <div className="flex justify-center">
+        <RadioGroup row>
+          <FormControlLabel value="hiragana" control={<Radio />} label="Hiragana" />
+          <FormControlLabel value="katakana" control={<Radio />} label="Katakana" />
+          <FormControlLabel value="kanji" control={<Radio />} label="Kanji" />
+        </RadioGroup>
+      </div>
+
+      <h2 className="text-4xl pt-4 font-serif">Please Enter a name:</h2>
       <h2 className="text-2xl font-serif">お名前を入力してください：</h2>
       <form
         onSubmit={async (e) => {
@@ -44,33 +52,31 @@ export default function Home() {
           navigate(`/kanji/${roomId}`);
         }}>
         <FormControl>
-          <div className="flex justify-center">
-            <RadioGroup row>
-              <FormControlLabel value="kanji" control={<Radio />} label="Kanji" />
-              <FormControlLabel value="grammar" control={<Radio />} label="Grammar" />
-            </RadioGroup>
-          </div>
           <TextField
             value={name}
             onChange={(e) => setName(e.target.value)}
-            placeholder="Enter your name"
+            placeholder="Name"
+            className="py-4 color-black"
           />
         </FormControl>
       </form>
+      <div className="pt-4" >Enter a 4-letter room code:</div>
       <form
         onSubmit={async (e) => {
           e.preventDefault();
           await joinRoom(player, roomId);
           navigate(`/kanji/${roomId}`);
         }}>
+
         <div className="flex flex-col">
           <TextField
             value={roomId}
             onChange={(e) => (roomId.length < 4 ? setRoomId(e.target.value) : null)}
-            placeholder="Enter 4 letter code" // TODO make sure this is 4 letters
+            placeholder="Code" // TODO make sure this is 4 letters
           />
-          <button type="submit">Join Room</button>
+          <button type="submit" className="pt-2">Join Room</button>
         </div>
+        
       </form>
     </div>
   );
